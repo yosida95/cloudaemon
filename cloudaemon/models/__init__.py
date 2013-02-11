f0ac467bd785932f67ef70694335ed6f9e39895a
yosida95 <kohei@yosida95.com>
2013-02-12 04:48:21 +0900

    Implement renderer of template; Reimplement TemplateMixin

--- cloudaemon/models/__init__.py
+++ cloudaemon/models/__init__.py
@@ -6,19 +6,6 @@ from xml.etree.ElementTree import (
 )
 
 
-class TemplateMixin(object):
-    TAG_NAME = None
-
-    def __init__(self, **attrib):
-        if self.TAG_NAME is None:
-            raise ValueError
-
-        self.root = TemplateElement(self.TAG_NAME, **attrib)
-
-    def tostring(self):
-        return tostring(self.root.render())
-
-
 class TemplateElement(object):
 
     def __init__(self, tag, **attrib):
@@ -76,6 +63,13 @@ class TemplateElement(object):
 
     text = property(get_text, set_text)
 
+    def copy(self):
+        inst = self.__class__(self.tag, **self.attrib)
+        inst._text = self._text
+        inst._children = [child.copy() for child in self._children]
+
+        return inst
+
     def render(self):
         root = Element(self.tag, attrib=self.attrib)
         if self._text is not None:
@@ -85,3 +79,64 @@ class TemplateElement(object):
             root.append(child.render())
 
         return root
+
+
+class TemplateMixin(object):
+    __tag_name__ = None
+    required_values = []
+
+    def __init__(self, **attrib):
+        if self.__tag_name__ is None:
+            raise ValueError(u'__tag_name__')
+
+        self._root = TemplateElement(self.__tag_name__, **attrib)
+
+        required_attributes = set(
+            key.split(u'__')[0] for key in self.required_values
+        )
+        for attribute in required_attributes:
+            if attribute not in dir(self):
+                raise ValueError(u'%s.%s is undefined' % (
+                    self.__class__.__name__, attribute
+                ))
+
+        self.attributes = {}
+        for key in dir(self):
+            value = getattr(self, key)
+            if key != u'_root' and\
+                    (isinstance(value, TemplateElement)
+                     or isinstance(value, TemplateMixin)):
+                self.attributes[key] = value.copy()
+                self._root.append(self.attributes[key])
+
+    def copy(self):
+        return self.__class__(**dict(self._root.items()))
+
+
+class TemplateRenderer(object):
+
+    def __init__(self, template):
+        self.template = template
+
+    def set_value(self, key, value, root=None):
+        if root is None:
+            root = self.template
+
+        key_splited = key.split(u'__')
+        if len(key_splited) > 2:
+            self.set_value(u'__'.join(key_splited[1:]), value,
+                           root.attributes[key_splited[0]])
+        else:
+            if key_splited[1] == u'$text':
+                root.attributes[key_splited[0]].set_text(value)
+            else:
+                root.attributes[key_splited[0]].set(key_splited[1], value)
+
+    def render(self, values):
+        if len(set(self.template.required_values) - set(values.keys())) > 0:
+            raise ValueError(u'Not enough values')
+
+        for key, value in values.items():
+            self.set_value(key, value)
+
+        return tostring(self.template._root.render())
